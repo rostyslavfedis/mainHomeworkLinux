983b6b4e934496cdd6d047d14d3068c77dd19d55
rostyslavfedis <rostyk110501@gmail.com>
2020-10-28 20:04:53 +0200

    js1

---
+++ homework5/class.js
@@ -0,0 +1,197 @@
+
+//task1
+
+// let txt1='hello';
+// let txt2='owu';
+// let txt3='ua';
+// let num1=1;
+// let num2=10;
+// let num3=-999;
+// let num4=123;
+// let num5=3.14;
+// let num6=2.7;
+// let num7=16;
+// let bool1=true;
+// let bool2=false;
+
+// console.log(txt1,txt2,txt3,num1,num2,num3,num4,num5,num6,num7,bool1,bool2);
+// document.write(txt1,txt2,txt3,num1,num2,num3,num4,num5,num6,num7,bool1,bool2);
+// alert(txt1,txt2,txt3,num1,num2,num3,num4,num5,num6,num7,bool1,bool2);
+
+//task2
+
+// txt1='hi';
+// txt2='mike';
+// txt3='den';
+// num1=5;
+// num2=1;
+// num3=6;
+// num4=15;
+// num5=345;
+// num6=322;
+// num7=43;
+// bool1=false;
+// bool2=true;
+// console.log(txt1,txt2,txt3,num1,num2,num3,num4,num5,num6,num7,bool1,bool2);
+// document.write(txt1,txt2,txt3,num1,num2,num3,num4,num5,num6,num7,bool1,bool2);
+// alert(txt1,txt2,txt3,num1,num2,num3,num4,num5,num6,num7,bool1,bool2);
+
+//task3
+
+// let fname="rostyk ";
+// let sname="fedis ";
+// let father="igor";
+// let dayb=11;
+// let monthb=05;
+// let yearb=2001;
+
+// console.log(fname,sname,father,dayb,monthb,yearb);
+// document.write(fname,sname,father,dayb,monthb,yearb);
+// alert(fname,sname,father,dayb,monthb,yearb);
+
+//task4
+
+// let firstName=prompt("Як тебе звати?");
+// let secondName=prompt("Твоє прізвище:");
+// let patronymics=prompt("По-батькові:");
+
+
+// console.log(firstName);
+// console.log(secondName);
+// console.log(patronymics);
+
+// document.write(firstName);
+// document.write(secondName);
+// document.write(patronymics);
+
+// alert(firstName);
+// alert(secondName);
+// alert(patronymics);
+
+//task5
+
+// let firstName=prompt("Як тебе звати?");
+// let secondName=prompt("Твоє прізвище:");
+// let patronymics=prompt("По-батькові:");
+// let person=firstName+secondName+patronymics;
+
+// console.log(person);
+// document.write(person);
+// alert(person);
+
+
+//task6
+
+// let firstName=prompt("Як тебе звати?");
+// let secondName=prompt("Твоє прізвище:");
+// let patronymics=prompt("По-батькові:");
+
+// let firstName2=prompt("Як тебе звати?");
+// let secondName2=prompt("Твоє прізвище:");
+// let patronymics2=prompt("По-батькові:");
+
+// let firstName3=prompt("Як тебе звати?");
+// let secondName3=prompt("Твоє прізвище:");
+// let patronymics3=prompt("По-батькові:");
+
+// let firstName4=prompt("Як тебе звати?");
+// let secondName4=prompt("Твоє прізвище:");
+// let patronymics4=prompt("По-батькові:");
+
+// let person1=secondName+firstName+patronymics;
+// let person2=secondName2+firstName2+patronymics2;
+// let person3=secondName3+firstName3+patronymics3;
+// let person4=secondName4+firstName4+patronymics4;
+// console.log(person1,person2,person3,person4);
+// document.write(person,person2,person3,person4);
+// alert(person,person2,person3,person4);
+
+//task7
+// let numOne=+prompt("enter number:");
+// let numTwo=+prompt("enter number:");
+// let numThree=+prompt("enter number:");
+
+// let res=numOne+numTwo+numThree;
+// console.log(res);
+
+//task8
+// let num1=parseInt(prompt("enter number:"));
+// let num2=parseInt(prompt("enter number:"));
+// let num3=parseInt(prompt("enter number:"));
+// let num4=parseInt(prompt("enter number:"));
+
+
+// let result=num1+num2+num3+num4;
+// console.log(result);let num1=parseInt(prompt("enter number:"));
+
+//task9
+
+// let num1=parseFloat(prompt("enter an integer:"));
+// let num2=parseFloat(prompt("enter an integer:"));
+// let num3=parseFloat(prompt("enter an integer:"));
+
+
+// let result=num1+num2+num3;
+// console.log(result);
+
+//task10
+
+// let num1=parseFloat(prompt("enter an integer:"));
+// let num2=parseFloat(prompt("enter an integer:"));
+// let num3=parseFloat(prompt("enter an integer:"));
+
+
+
+// let result=Math.round(num1)+Math.round(num2)+Math.round(num3);
+// console.log(result);
+
+//task11
+
+// let numMain=+prompt("enter main number:");
+// let numPow=+prompt("enter pow number:");
+
+// numMain=Math.round(numMain);
+// numPow=Math.round(numPow);
+
+// let result=Math.pow(numMain,numPow);
+// console.log(result);
+
+//task12
+// let a = 100;
+// let b = '100';
+// let c = true; 
+// let d = undefined;
+
+
+// console.log(typeof(a));
+// console.log(typeof(b));
+// console.log(typeof(c));
+// console.log(typeof(d));
+
+//task13
+let a = 5 < 6;
+let b = 5 == 6;
+let c = 5 >6;
+let d = 5 >= 6 ;
+let e = 10 <= 10; 
+let f = 10 == 10 ;
+let g = 10 > 10 ;
+let k = 10 < 10 ;
+let l = 10 != 10 ;
+let p = 123 === '123'; 
+let o = 123 == '123' ; 
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(f);
+console.log(g);
+console.log(k);
+console.log(l);
+console.log(p);
+console.log(o);
+
+
+
